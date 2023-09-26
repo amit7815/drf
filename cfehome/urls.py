cf64f67d066a9908dd3d23b93276b650f3cbf523
amit7815 <yadavamit14801@gmail.com>
2023-09-26 22:57:58 +0530

    generics.RetrieveAPIView and serializer

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))   # this path will be like localhost:8000/api/
+    path('api/', include('api.urls')) ,  # this path will be like localhost:8000/api/
+    path('api/products/', include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer 
 
-# Create your views here.
+# Genreic API View
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+    # Product.objects.get(pk=15) here pk is look_up field
+
+
+product_detail_view = ProductDetailAPIView.as_view()   # we can do like this as well but .as_view() is used by everyone
