6fa9a822ba27d092524be4c7b5940ecffb553a53
amit7815 <yadavamit14801@gmail.com>
2023-09-27 22:24:48 +0530

    function based view for creating api

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.product_list_create_view),
     path('<int:pk>/', views.product_detail_view)  # here pk is actual field name
+    # path('', views.product_alt_view),
+    # path('<int:pk>/', views.product_alt_view)
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django .http import Http404
+from django.shortcuts import get_object_or_404
 
 # Genreic API View
 
@@ -10,12 +14,12 @@ from .serializers import ProductSerializer
 
 #     def perform_create(self, serializer):
 #         print(serializer.validated_data)
-#         title = serializer.validated_data.get("title")
-#         # serializer.save()  when content is not present
-#         content = serializer.validated_data.get("content") or None
-#         if content is None:
-#             content = title
-#         serializer.save(content=content)    
+        # title = serializer.validated_data.get("title")
+        # # serializer.save()  when content is not present
+        # content = serializer.validated_data.get("content") or None
+        # if content is None:
+        #     content = title
+        # serializer.save(content=content)    
 #         # send a django signal
 
 # product_create_view = ProductCreateAPIView.as_view()
@@ -46,3 +50,36 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 
 product_list_create_view = ProductListCreateAPIView.as_view()   # we can do like this as well but .as_view() is used by everyone
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method  # PUT --> uupdate, DESTROY --> delete
+
+    if method == 'GET':
+        if pk is not None:
+            # queryset = Product.objects.filter(pk=pk)
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+            # if not queryset.exist():
+            #     raise Http404
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+        # url_args
+        # get request  --> detail view
+        # list view
+
+    if method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):  # for knowing the detailed error
+            title = serializer.validated_data.get("title")
+            # serializer.save()  when content is not present
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=404) 
+        # create an item
+
