4a4465c71fe0d0c035957c71b9e8ac2efa24cd9c
amit7815 <yadavamit14801@gmail.com>
2023-09-27 22:47:23 +0530

    generic destoryapiview and updateapiview

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list_create_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/destroy/', views.product_delete_view),
     path('<int:pk>/', views.product_detail_view)  # here pk is actual field name
     # path('', views.product_alt_view),
     # path('<int:pk>/', views.product_alt_view)
--- products/views.py
+++ products/views.py
@@ -51,6 +51,35 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()   # we can do like this as well but .as_view() is used by everyone
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+    # Product.objects.get(pk=15) here pk is look_up field
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+product_update_view = ProductUpdateAPIView.as_view()   # we can do like this as well but .as_view() is used by everyone
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+    # Product.objects.get(pk=15) here pk is look_up field
+    
+    def perform_destroy(self, instance):
+        # instance
+        super().perform_destroy(instance)
+
+
+product_delete_view = ProductDestroyAPIView.as_view()
+
+
+
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method  # PUT --> uupdate, DESTROY --> delete
